5d7c595bc2927e1c2587fb02f6d1eba19fed0743
gusboling <geekish2428@gmail.com>
2017-01-28 13:18:02 -0800

    Fleshed out most of expressions and some basic debugging messages.

--- phase2/syntax.cpp
+++ phase2/syntax.cpp
@@ -27,6 +27,11 @@ void xprsAND();
 void xprsEQUAL();
 void xprsNOTEQUAL();
 void xprsINEQUAL();
+void xprsPLMN();
+void xprsMDR();
+void xprsOPS();
+void xprsINDEX();
+void xprsLAST();
 
 string lexbuf; //Buffer for token-text
 int lookahead; //Variable for storing token #'s
@@ -42,45 +47,144 @@ void match(int token){
 	}  
 }
 
-//FUNCTION: "xprsOR" -> expression-OR
-//PRE: lookahead is an integer
+//Rule for "||" expressions
 void xprsOR(){
-	xprsAND();
+	xprsAND(); //Recurse
 	while(lookahead == OR){
-		match(OR);
+		match(OR); //Advance the lexer
 		cout << "or" << endl;
 		xprsAND();
 	}
 }
 
+//Rule for "&&" expressions
 void xprsAND(){
-	xprsEQUAL();
+	xprsEQUAL(); //Recurse
 	while(lookahead == AND){
-		match(AND);
+		match(AND); //Advance the lexer
 		cout << "and" << endl;
 		xprsEQUAL();
 	}
 }
 
+//Rule for "==" and "!=" expressions
 void xprsEQUAL(){
-	xprsNOTEQUAL();
-	while(lookahead == EQUAL){
-		match(EQUAL);
-		cout << "eql" << endl;
-		xprsNOTEQUAL();
-	}
-}
-
-void xprsNOTEQUAL(){
 	xprsINEQUAL();
-	while(lookahead == NOTEQUAL){
-		match(NOTEQUAL);
-		cout << "neq" << endl;
-		xprsINEQUAL();
+	while( (lookahead == EQUAL) || (lookahead == NOTEQUAL) ){
+		if(lookahead == EQUAL){
+			match(EQUAL);
+			cout << "eql" << endl;
+			xprsINEQUAL();
+		}
+		else{
+			match(NOTEQUAL);
+			cout << "neq" << endl;
+			xprsINEQUAL();
+		}			
 	}
 }
 
+//Rule for "<", "<", ">=", and ">" expressions
 void xprsINEQUAL(){
+	xprsPLMN();
+	while( (lookahead == GTN) || (lookahead == LTN) || (lookahead == GREATERTHANOREQUAL) || (lookahead == LESSTHANOREQUAL) ) { //TODO: Should this be an "if" statement instead of a loop?
+		if(lookahead == GTN){
+			match(GTN);
+			cout << "gtn" << endl;
+			xprsPLMN();
+		}
+		else if(lookahead == LTN){
+			match(LTN);
+			cout << "ltn" << endl;
+			xprsPLMN();
+		}
+		else if(lookahead == GREATERTHANOREQUAL){
+			match(GREATERTHANOREQUAL);
+			cout << "geq" << endl;
+			xprsPLMN();
+		}
+		else{
+			match(LESSTHANOREQUAL);
+			cout << "leq" << endl;
+			xprsPLMN();
+		}
+	}
+}
+
+//Rule for "+" and "-" expressions
+void xprsPLMN(){
+	xprsMDR();
+	while( (lookahead==ADD) || (lookahead==SUB) ){
+		if(lookahead == ADD){
+			match(ADD);
+			cout << "add" << endl;
+			xprsMDR();
+		}
+		else{
+			match(SUB);
+			cout << "sub" << endl;
+			xprsMDR();
+		}	
+	}
+}
+
+//Rule for "*", "/", and "%" expressions [(M)ultiplication, (D)ivision, (R)emainder]
+void xprsMDR(){
+	xprsOPS();
+	while( (lookahead == MUL) || (lookahead == DIV) || (lookahead == REM)){
+		if(lookahead == MUL){
+			match(MUL);
+			cout << "mul" << endl;
+			xprsOPS();
+		}
+		else if(lookahead == DIV){
+			match(DIV);
+			cout << "div" << endl;
+			xprsOPS();
+		}
+		else{
+			match(REM);
+			cout << "rem" << endl;
+			xprsOPS();
+		}
+	}
+}
+
+void xprsOPS(){
+	xprsINDEX();
+	while( (lookahead=='&') || (lookahead=='*') || (lookahead=='!') || (lookahead=='-') || (lookahead==SIZEOF) ){ //TODO: straighten out what these equivalences should be WRT tokens.h
+		if(lookahead == '&'){
+			match('&');
+			cout << "addr" << endl;
+			xprsINDEX();
+		}
+		else if(lookahead == '*'){
+			match('*');
+			cout << "deref" << endl;
+			xprsINDEX();	
+		}
+		else if(lookahead == '!'){
+			match('!');
+			cout << "not" << endl;
+			xprsINDEX();
+		}
+		else if(lookahead == '-'){
+			match('-');
+			cout << "neg" << endl;
+			xprsINDEX();
+		}
+		else{
+			match(SIZEOF);
+			cout << "sizeof" << endl;
+		}	
+	}
+}
+
+void xprsINDEX(){
+	//TODO
+}
+
+void xprsLAST(){
 	//TODO
 }
 
@@ -91,9 +195,10 @@ void statement(){
 
 int main(){
 	lookahead = lexan(lexbuf);
+	cerr << "Found token: " << lookahead << endl;
 	while(lookahead != DONE){
-		cerr << "Found token: " << lexbuf << endl;
 		lookahead = lexan(lexbuf);
+		cerr << "Found token: " << lookahead << endl;
 	}	
 	return 0;
 }
