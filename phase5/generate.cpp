29484a98a8ab6a45ed9f17fd0d671349a86323d8
gusboling <geekish2428@gmail.com>
2017-03-05 19:06:09 -0800

    Added function prologue and epilogue

--- phase5/generate.cpp
+++ phase5/generate.cpp
@@ -3,11 +3,28 @@
 using namespace std;
 
 void Function::generate(){
+	int offset = 0;
+	allocate(offset);
+	
+	//START Function Prologue
+	cout << ".globl\t" << _id->name() << endl;
+	cout << _id->name() << ":" << endl;
+	cout << "pushl\t%ebp" << endl;
+	cout << "movl\t%esp, %ebp" << endl;
+	cout << "subl\t$" << -_offset << ",%ebp" << endl;
+	//END Function Prologue
 
+	_body->generate();
+
+	//START Function Epilogue
+	cout << "movl\t%ebp,%esp" << endl;
+	cout << "popl\t%ebp" << endl;
+	cout << "ret" << endl << endl;
+	//END Function Epilogue
 }
 
 void Block::generate(){
-
+	//TODO: What the hell goes here?
 }
 
 void Call::generate(){
