d97b8ada3b677227fade5ab26326f735a345e980
gusboling <geekish2428@gmail.com>
2017-03-06 19:50:05 -0800

    Added test dirs, test script

--- phase5/generate.cpp
+++ phase5/generate.cpp
@@ -1,17 +1,28 @@
+//Libraries
+#include <iostream>
+#include <sstream>
+
+//Program Headers
 #include "Tree.h"
 
 using namespace std;
 
-void Function::generate(){
+/*
+ * Function:	function::generate()
+ *
+ * Description: Output the appropriate object code for the given function object
+ */
+void Function::generate()
+{
 	int offset = 0;
-	allocate(offset);
+	allocate(offset);//Updates the value of offset
 	
 	//START Function Prologue
 	cout << ".globl\t" << _id->name() << endl;
 	cout << _id->name() << ":" << endl;
 	cout << "pushl\t%ebp" << endl;
 	cout << "movl\t%esp, %ebp" << endl;
-	cout << "subl\t$" << -_offset << ",%ebp" << endl;
+	cout << "subl\t$" << -offset << ",%ebp" << endl;
 	//END Function Prologue
 
 	_body->generate();
@@ -23,24 +34,50 @@ void Function::generate(){
 	//END Function Epilogue
 }
 
-void Block::generate(){
-	//TODO: What the hell goes here?
+/*
+ * Function:	Block::generate()
+ *
+ * Description: Cycles through block statements (either function calls or assignments) and call the 
+ * associated generate function.
+ */
+void Block::generate()
+{
+	for(size_t i = 0; i < _stmts.size(); i++)
+	{
+		//For each statement pointer, call its associated generate method
+		_stmts[i]->generate();
+	}
 }
 
-void Call::generate(){
-
+void Call::generate()
+{
+	for(size_t i = _args.size(); i < 0; i--)
+	{
+		_args[i]->generate();
+		cout << "pushl\t" << _args[i]->_location << endl;
+	}
+	
+	cout << "call\t" << _id->name() << endl;
 }
 
 void Identifier::generate(){
+	stringstream conversionStream;
+	conversionStream << symbol()->_offset;
+	string stringOffset = conversionStream.str();
 
+	if(_symbol->_offset == 0) _location = _symbol->name(); //For global variables
+	else _location = stringOffset + "(%ebp)";
 }
 
 void Number::generate(){
-
+	//Update inherited location member to literal value
+	_location = "$" + value();
 }
 
 void Assignment::generate(){
-
+	//The value of the right operand replaces that of the object referred to by the lvalue of the left operand	
+	cout << "movl\t" << _right->_location << ",%eax" << endl; 
+	cout << "movl\t%eax," << _left->_location << endl;
 }
 
 
