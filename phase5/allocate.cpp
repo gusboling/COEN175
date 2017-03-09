39de25ad39f13f484c91684ee27c81162dd6c826
gusboling <geekish2428@gmail.com>
2017-03-08 21:41:26 -0800

    Phase5 submission commit

--- phase5/allocate.cpp
+++ phase5/allocate.cpp
@@ -11,38 +11,37 @@
 using namespace std;
 
 void Function::allocate(int &offset){
-	int osResult = 8; //Initialize offset equal to 4
+	int osResult = 8; 
 	Symbols functionSyms = _body->declarations()->symbols(); //Get vector of function symbols from Block member	
 	
 	//Calculate offset for function parameters
 	int numParams = _id->type().parameters()->size();
+	int numLocals = functionSyms.size();
 
 	for(int i=0; i < numParams; i++) 
 	{
 		functionSyms[i]->_offset = osResult;		
-		osResult = osResult + 4; //For each parameter, incremement offset by 4
+		osResult = osResult + 4; 
 	}
 
-	osResult = 0; //Reset offset 
+	osResult = 0;  
 
 	//Calculate offset for function body
-	for(unsigned i=0; i < functionSyms.size(); i++)
+	for(int i=numParams; i < numLocals; i++)
 	{
 		if( functionSyms[i]->type().isInteger() || functionSyms[i]->type().isPointer() )
 		{
 			osResult = osResult - 4;
 			functionSyms[i]->_offset = osResult; 
-			//cerr << "[ALLOCATE][FUNC_SYM][INT|PTR] name: " << functionSyms[i]->name() << " offset: " << functionSyms[i]->_offset << endl;
 		}
-		else
+		else if( functionSyms[i]->type().isArray() )
 		{
-			int arrayOffset = functionSyms[i]->type().length() * 4; //Is this right? Ask Gabe.
-			//cerr << "[ALLOCATE][FUNC_SYM][ARRAY] name: " << functionSyms[i]->name() << " offset: " << functionSyms[i]->_offset << endl;
+			int arrayOffset = functionSyms[i]->type().length() * 4; 
 			osResult = osResult - arrayOffset;
 			functionSyms[i]->_offset = offset;
 		}
 
 	}
 
-	offset = osResult; //Set function symbol-offset to calculated offset result
+	//offset = osResult; //Set function symbol-offset to calculated offset result
 }
